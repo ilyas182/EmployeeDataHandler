d288dc67d2fec9fb1766cc1d1b4ffbddd00b49eb
Ilyas Effendi <ilyas2104@gmail.com>
2025-08-01 15:47:16 +0800

    create invoker class and command interface

---
+++ src/main/java/Invoker.java
@@ -0,0 +1,12 @@
+import java.util.Stack;
+
+public class Invoker {
+    private Command[] cmdToExecute;
+
+    public void setCommandsForExecution(Command[] command) {
+    }
+
+    public void executeCommand(Stack<Command> history){
+
+    }
+}
